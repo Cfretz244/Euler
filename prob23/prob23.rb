ea0546837dfffd0eba34786eb34809e42ca8da7b
root <root@christopherfretz.com>
2014-04-18 20:21:42 +0000

    routine commit

---
+++ prob23/prob23.rb
@@ -0,0 +1,43 @@
+require 'set'
+
+def divisorSum term
+    sqrt = Math.sqrt term
+    sum = 0
+    for i in 1..sqrt
+        if term % i == 0
+            i != 1 ? counter = term / i : counter = 0
+            sum += i + counter
+        end
+    end
+    sum
+end
+
+abundants = Set.new
+pairs = Set.new
+composites = Set.new
+results = Set.new
+for i in 12..28123
+    if i < divisorSum(i)
+        abundants.add i
+    end
+    results.add i
+end
+puts abundants.size
+puts results.size
+for elem1 in abundants
+    for elem2 in abundants
+        sym = "#{[elem1, elem2].min}|#{[elem1, elem2].max}".to_sym
+        if not pairs.include? sym
+            pairs.add sym
+            composites.add(elem1 + elem2)
+        end
+    end
+    puts elem1
+end
+
+results.delete_if {|elem| composites.include? elem}
+total = 0
+for result in results
+    total += result
+end
+puts total
