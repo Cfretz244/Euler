d5cc0f12885d3e04e6a3c859bf481df99f0d56fa
Chris Fretz <--global>
2014-04-23 14:11:51 +0000

    solved prob25

---
+++ prob25/prob25.rb
@@ -0,0 +1,11 @@
+fibTerm = 3
+prev2 = 1
+prev = 1
+current = 2
+until current.to_s.size >= 1000
+    prev2 = prev
+    prev = current
+    current = prev2 + prev
+    fibTerm += 1
+end
+puts fibTerm
