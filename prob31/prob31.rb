c071a5e02ac33e22527310cc2dea16831fbab434
Chris Fretz <Cfretz@icloud.com>
2014-05-31 06:45:09 +0000

    routine commit

--- prob31/prob31.rb
+++ prob31/prob31.rb
@@ -1,29 +1,32 @@
-require 'debugger'; debugger
+require 'byebug'
 $values = [100, 50, 20, 10, 5, 2, 1]
 
 def maxMult deficit, term
     total = 0
     count = 0
     until total >= deficit
-        total += values[term]
+        total += $values[term]
         count += 1
     end
-    count
+    total > deficit ? count - 1 : count
 end
 
 $combinations = 0
 def permute deficit, term
-    return if term == keys.size and deficit != 0
+    return if term == $values.size and deficit != 0
     mult = maxMult deficit, term
-    until mult == 0
-        if deficit - (mult * values[term]) == 0
+    until mult <= 0
+        if deficit - (mult * $values[term]) == 0
             $combinations += 1
             mult -= 1
         end
-        deficit -= values[$term] * mult
+        deficit -= $values[term] * mult
         permute deficit, term + 1
+        deficit += $values[term] * mult
+        mult -= 1
     end
 end
 
+byebug
 permute 200, 0
 puts $combinations
